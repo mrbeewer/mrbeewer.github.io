1a445ab9eb8c69ff59098d728b52afc76475086c
David Beeler <david.beeler@gmail.com>
2015-11-09 16:03:01 -0600

    Initial Commit

---
+++ project1/scripts/app.js
@@ -0,0 +1,67 @@
+var canvas = CE.defines("canvas_id").ready(function() {
+
+  canvas.Scene.call("MyScene");
+
+});
+
+canvas.Scene.New({
+  name: "MyScene", // Need this!
+  materials: { // loads images, sounds, videos see materials.load()
+    images: {
+            // For CanvasEngine load "bar" first, we add index property
+            "bar": "styles/circle.png"
+		}
+  },
+  // all these methods are optional
+  called: function(stage) {
+    // initialize an element
+    this.el = this.createElement();
+    stage.append(this.el);
+  },
+
+  preload: function(stage, pourcent, material) {
+    this.el.drawImage("bar", 0, 0, pourcent + "%");
+  },
+
+  ready: function(stage, params) {
+    // use stage.empty() for clear stage
+  }
+
+});
+//
+// canvas.Scene.call("MyScene")
+
+
+/**
+    Example to create a progress bar
+*/
+
+// var canvas = CE.defines("canvas_id").
+// 		ready(function() {
+// 			canvas.Scene.call("MyScene");
+//         });
+//
+// canvas.Scene.new({
+// 	name: "MyScene",
+// 	materials: {
+//         // Usually put relatives links
+// 		images: {
+//             // For CanvasEngine load "bar" first, we add index property
+//             "bar": {path: "http://rsamaium.github.io/CanvasEngine/samples/preload/images/bar_full.jpg", index: 0},
+//             "1": "http://rsamaium.github.io/CanvasEngine/samples/preload/images/1.jpg",
+//             "2": "http://rsamaium.github.io/CanvasEngine/samples/preload/images/2.jpg",
+//             "3": "http://rsamaium.github.io/CanvasEngine/samples/preload/images/3.jpg"
+// 		}
+// 	},
+// 	called: function(stage) {
+//         // Initialize an element
+// 		this.el = this.createElement();
+// 		stage.append(this.el);
+// 	},
+// 	preload: function(stage, pourcent, material) {
+// 		this.el.drawImage("bar", 0, 0, pourcent + "%");
+// 	},
+// 	ready: function(stage) {
+//         // use stage.empty() for clear stage
+// 	}
+// });
